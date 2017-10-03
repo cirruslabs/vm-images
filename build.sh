a3a2342cc4cca6439bb6d370ab836cdad66293b0
fedor <fedor.korotkov@gmail.com>
2017-10-03 09:18:36 -0700

    build image with packer

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-packer build \
+packer build -force \
     -var 'gcp_project_id=cirrus-images-latest' \
     -var 'gce_base_image=ubuntu-1604-xenial-v20170919' \
     -var 'java_version=8' \
-    googlecompute/java.json
+    googlecompute/ubuntu_images.json
