f7f95f9717206d8972d7acc0b78d1bd1cbd72c72
fedor <fedor.korotkov@gmail.com>
2018-02-09 08:19:45 -0500

    Build Windows Node images

--- build.sh
+++
@@ -1,5 +0,0 @@
-packer build -force \
-    -var 'gcp_project_id=cirrus-images-latest' \
-    -var 'gce_base_image=ubuntu-1604-xenial-v20170919' \
-    -var 'java_version=8' \
-    googlecompute/ubuntu_images.json
